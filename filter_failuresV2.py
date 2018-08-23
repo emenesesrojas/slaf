a61f8cb0574509dac83d600b600addcf7acf040b
elvinrz <erojas@una.cr>
2018-08-23 13:21:22 -0700

    filter failure works ok

--- filter_failuresV2.py
+++ filter_failuresV2.py
@@ -15,18 +15,19 @@ import time
 class FailedJob:
 	def __init__(self, line):
 		self.line = line
-		self.nodes = []
+		self.nodes = {}
 	
-def failureFilter(fileName):
+def failureFilter(fileName, outputFileName):
 	jobs_fail = {}
-	formatAlt = '%Y-%m-%d %H:%M:%S'
-  
+	#formatAlt = '%Y-%m-%d %H:%M:%S'
+	file = open(outputFileName, 'w')
+
 	#size of failure file
 	with open(fileName) as f:
 		lines = len(f.readlines())
 		
 	count = 0 
-	
+	file.write("| hostname | job_id  | fail_time           | category | reason | description                      | text                                                                                                                                  | jf_id   |		nodes\n")
 	with open(fileName) as f:
 		next(f)
 		for line in f:
@@ -35,9 +36,7 @@ def failureFilter(fileName):
 	
 			fields = line.split('|')
 			jobid = int(fields[2].strip())
-			ftime = fields[3].strip() #before ftime
-			#failureTime = time.strptime(ftime, formatAlt)
-			#epoch = time.mktime(failureTime)
+			#ftime = fields[3].strip() 
 			category = fields[4].strip()
 			reason = fields[5].strip()
 			description = fields[6].strip()
@@ -55,40 +54,42 @@ def failureFilter(fileName):
 			else:	
 				node =  match.group(0)
             
-			
-			if jobid in jobs_fail and category in jobs_fail[jobid] and description in jobs_fail[jobid][category] and reason in jobs_fail[jobid][category][description]:
-				jobs_fail[jobid][category][description][reason].nodes.append(node)
-				# print(node)
-				print(len(jobs_fail[jobid][category][description][reason].FailedJob.nodes))
-			
+			if node != '':
+				if jobid in jobs_fail and category in jobs_fail[jobid] and description in jobs_fail[jobid][category] and reason in jobs_fail[jobid][category][description]:
+					if node not in jobs_fail[jobid][category][description][reason].nodes.keys():
+						jobs_fail[jobid][category][description][reason].nodes[node]={}
+					
 			entry = FailedJob(line)
 			#create entry
 			if jobid not in jobs_fail:
 				jobs_fail[jobid] = {}
 			if category not in jobs_fail[jobid]:
 				jobs_fail[jobid][category] = {}
-			if description not in jobs_fail[jobid]:
+			if description not in jobs_fail[jobid][category]:
 				jobs_fail[jobid][category][description] = {}
 			if reason not in jobs_fail[jobid][category][description]:
 				jobs_fail[jobid][category][description][reason] = entry
-				
+				jobs_fail[jobid][category][description][reason].nodes[node] = {}
+	
+	entry_nodes = ""
 	for id in jobs_fail.keys():
 		for cat in jobs_fail[id].keys():
 			for des in jobs_fail[id][cat].keys():
 				for rea in jobs_fail[id][cat][des].keys():
-					print(jobs_fail[id][cat][des][rea].line)
-					for i in range(len(jobs_fail[id][cat][des][rea].nodes)):
-						print("......................"+jobs_fail[id][cat][des][rea].nodes[i])
-
+					#for i in jobs_fail[id][cat][des][rea].nodes.keys():
+					#	entry_nodes = entry_nodes +  i #jobs_fail[id][cat][des][rea].nodes[i].keys()
+					##print("##################################################################")
+					entry_nodes = "|"+" ".join(jobs_fail[id][cat][des][rea].nodes.keys()) +"\n"#" ".join(jobs_fail[id][cat][des][rea].nodes.keys()) + "\n"
+					# print(jobs_fail[id][cat][des][rea].line.strip() + entry_nodes)
+					# sys.exit()
+					file.write(jobs_fail[id][cat][des][rea].line.strip() + entry_nodes)
+	file.close()
 
 if len(sys.argv) > 2:
 	fileName = sys.argv[1]
 	outputFileName =  sys.argv[2]
-	file = open(outputFileName, 'w')
+	failureFilter(fileName, outputFileName)
 	
-	file.write("|host name  |Job ID  |   FailTime            |   Category  |  Description   |   StartTime   |   EndTime   |    Nodes")
-	failureFilter(fileName)
-	file.close()
 else:	
 	print ("ERROR, usage: %s <file><output file>" % sys.argv[0])
 	print ("<file>: Failure log file")
